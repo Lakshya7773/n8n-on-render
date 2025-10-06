aead091690702d52194f2949db2af31ce0874c2f
Lakshya7773 <146054751+Lakshya7773@users.noreply.github.com>
2025-10-06 19:11:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import { exec } from "child_process";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 exec("n8n start", (error, stdout, stderr) => {
   console.log(stdout);
   console.error(stderr);
